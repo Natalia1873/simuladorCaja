f3f84a827924d47ea7abdb5250d502c6622bcf52
Natalia Tsyleva <tsylevanatalia@gmail.com>
2024-07-31 13:13:41 +0200

    feat: ha creado estructura, primeros pasos en vistas

---
+++ app/view.py
@@ -0,0 +1,29 @@
+from simple_screen import locate, DIMENSIONS, Input
+
+class VistaTituloPagina:
+    def __init__(self, texto: str, y: int = 0):
+        self.texto = texto
+        self.y = y
+
+    def paint(self):
+        x = (DIMENSIONS.w - len(self.texto)) // 2
+        locate(x, self.y, self.texto)
+
+class VistaCatalogo:
+    def __init__(self, productos, x:int, y:int, w:int, num_filas:int):
+        self.peliculas = productos
+        self.x = x
+        self.y = y
+        self.w = w
+        self.num_filas = num_filas
+
+    def paint(self):
+        locate(self.x, self.y, "Codigo")
+        locate(self.x + 15, self.y, "|Producto")
+        locate(self.x + 30, self.y, "|Precio")
+        locate(self.x + 45, self.y, "|Unidades")
+        locate(self.x + 60, self.y, "|Total")
+
+        locate(self.x, self.y + 1, "---------------+--------------+--------------------------------------------------")
+
+        
